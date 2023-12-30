df7963db711bdcfeab8a849afc8169d78a3ff71a
Tarkeshwar Kumar <ktarkeshwar305@gmail.com>
2023-12-30 15:52:20 +0530

    Refactored handlers

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -1,12 +1,12 @@
 package handlers
 
-func login(w http.ResponseWriter, r *http.Request){
+func Login(w http.ResponseWriter, r *http.Request){
 	w.Header().Set("Content-Type", "text/html")
 	tmpl, _ := template.ParseFiles("./ui/leave_application.html")
 	tmpl.Execute(w, nil)
 }
 
-func leaveRecords(w http.ResponseWriter, r *http.Request) {
+func LeaveRecords(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "text/html")
 	tmpl, _ := template.ParseFiles("./ui/leave_records.html")
 	tmpl.Execute(w, nil)
--- main.go
+++ main.go
@@ -3,15 +3,15 @@ package main
 import (
 	"html/template"
 	"net/http"
-	
+	"github.com/Tarkeshwar-kumar/LeaveManagementSystem/handlers/handlers"
 )
 
 func main() {
 	mux := http.NewServeMux()
  	file := http.FileServer(http.Dir("./ui/"))
  	mux.Handle("/", file)
-	mux.HandleFunc("/login", login)
-	mux.HandleFunc("/my-leaves", leaveRecords)
+	mux.HandleFunc("/login", Login)
+	mux.HandleFunc("/my-leaves", LeaveRecords)
 	server := &http.Server{
 		Addr: "0.0.0.0:8080",
 		Handler: mux,
